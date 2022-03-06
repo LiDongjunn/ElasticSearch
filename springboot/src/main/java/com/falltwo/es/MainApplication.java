3efa048f30c7575b928c785fe340aa8083758953
lidongjunn <lidongjunn@163.com>
2022-03-06 23:20:45 +0800

    ES SpringBoot集成测试 v0.03

---
+++ springboot/src/main/java/com/falltwo/es/MainApplication.java
@@ -0,0 +1,16 @@
+package com.falltwo.es;
+
+/**
+ * @author: FallTwo
+ * @createTime: 2022/3/5 22:06
+ * @description:
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
